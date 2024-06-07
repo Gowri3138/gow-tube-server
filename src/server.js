62fe9386e325565a56b7780e81824dfcc4f58393
Gowri3138 <gowrigowri3138@gmail.com>
2024-06-07 10:24:59 -0400

    updated 1

--- src/server.js
+++ src/server.js
@@ -20,13 +20,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-// static files
-app.use(express.static("assets"));
-app.use("/api/medias/banners", express.static(__dirname + "/assets/banners"));
-app.use("/api/medias/profiles", express.static(__dirname + "/assets/profiles"));
-app.use("/api/medias/covers", express.static(__dirname + "/assets/covers"));
-app.use("/api/medias/videos", express.static(__dirname + "/assets/videos"));
-
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/channels", channelRoutes);
