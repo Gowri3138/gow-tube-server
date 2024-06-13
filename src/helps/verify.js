8dd80551e34e5ad22ce7b103dd642db5d8889ff4
logesh03-git <logeahlogesh06@gmail.com>
2024-06-13 05:51:47 -0400

    updated services1

--- src/helps/verify.js
+++ src/helps/verify.js
@@ -1,17 +1,28 @@
 const jwt = require("jsonwebtoken");
 const logger = require("../logger/logger");
+const Cookies = require('cookies');
 
 const verifyToken = (req, res, next) => {
   logger.info('Headers:', req.headers);
-  
+
+  let token = null;
+
+  // Check if the authorization header contains a Bearer token
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    logger.warn('No token found in Authorization header');
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    token = authHeader.split(' ')[1];
+  } else {
+    // If no Authorization header, check cookies for accessToken
+    const cookies = new Cookies(req, res);
+    token = cookies.get('accessToken');
+  }
+
+  if (!token) {
+    logger.warn('No token found in Authorization header or cookies');
     return res.status(401).json("You are not authenticated.");
   }
 
-  const token = authHeader.split(' ')[1];
-
+  // Verify the token
   jwt.verify(token, process.env.SECRET_JWT, (error, user) => {
     if (error) {
       logger.error('Token verification failed:', error.message);
@@ -24,3 +35,4 @@ const verifyToken = (req, res, next) => {
 };
 
 module.exports = verifyToken;
+
